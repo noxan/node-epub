67f996e9e5136d2972aaa7fad1f3ea41356f31b9
Richard <richard@stromer.org>
2020-02-12 12:15:25 +0100

    remove manifest check from main test case

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,7 +5,6 @@ describe('node-epub', () => {
     const epub = new EPub('./examples/lorem-ipsum-pages.epub');
     expect(epub.version).toEqual('3.0');
     expect(epub.metadata).toMatchSnapshot();
-    expect(epub.manifest).toMatchSnapshot();
     expect(epub.spine).toMatchSnapshot();
     expect(epub.guide).toMatchSnapshot();
   });
