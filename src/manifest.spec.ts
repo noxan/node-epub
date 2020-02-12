67ca697545c965ba5232121f91373b13d81e29b9
Richard <richard@stromer.org>
2020-02-12 12:31:55 +0100

    add test case with full manifest fixture

--- src/manifest.spec.ts
+++ src/manifest.spec.ts
@@ -1,8 +1,9 @@
 import fixutureMini from './manifest.fixture-mini.json';
+import fixutureFull from './manifest.fixture-full.json';
 import { parseManifest } from './manifest';
 
 describe('manifest', () => {
-  it('transform manifest fixture into key based object', () => {
+  it('transform manifest minimal fixture into key based object', () => {
     const manifest = parseManifest(fixutureMini);
     expect(manifest).toEqual({
       cover: {
@@ -11,5 +12,10 @@ describe('manifest', () => {
         mediaType: 'application/xhtml+xml',
       },
     });
+  });
+
+  it('transform manifest full fixture into key based object', () => {
+    const manifest = parseManifest(fixutureFull);
+    expect(manifest).toMatchSnapshot();
   });
 });
