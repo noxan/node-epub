c6280201979b8bfdc199566c123fabeb87594be3
Richard <richard@stromer.org>
2020-02-14 16:38:09 +0100

    rename Lorem Ipsum.epub

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,7 +2,7 @@ import { EPub } from './index';
 
 describe('node-epub', () => {
   it('should read epub metadata from file', () => {
-    const epub = new EPub('./examples/lorem-ipsum-pages.epub');
+    const epub = new EPub('./examples/Lorem Ipsum.epub');
     expect(epub.version).toEqual('3.0');
     expect(epub.metadata).toMatchSnapshot();
     expect(epub.spine).toMatchSnapshot();
@@ -10,7 +10,7 @@ describe('node-epub', () => {
   });
 
   it('should find the cover image', () => {
-    const epub = new EPub('./examples/lorem-ipsum-pages.epub');
+    const epub = new EPub('./examples/Lorem Ipsum.epub');
     expect(epub.getCoverImage()).toEqual({
       href: 'images/cover-image.png',
       id: 'cover-image',
