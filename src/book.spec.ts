ab0c7044e816f916448051179e49c7ffde26d52a
Richard <richard@stromer.org>
2020-02-15 15:54:58 +0100

    extract paths in test cases

--- src/book.spec.ts
+++ src/book.spec.ts
@@ -3,22 +3,23 @@ import fs from 'fs';
 import Book from './book';
 
 describe('epub30-test-0100 - Tests for Content Documents in a reflowable context', () => {
+  const epubFolderPath = './testsuite/content/30/epub30-test-0100';
+
   it('should read unpacked epub from folder path', () => {
-    const epubFolderPath = './testsuite/content/30/epub30-test-0100';
     const book = new Book(epubFolderPath);
     expect(book.isZipFile).toEqual(false);
   });
 });
 
 describe('Lorem ipsum - Epub zip-file created with Apple Pages', () => {
+  const epubFilePath = './examples/Lorem ipsum.epub';
+
   it('should read epub zip from file path', () => {
-    const epubFilePath = './examples/Lorem ipsum.epub';
     const book = new Book(epubFilePath);
     expect(book.isZipFile).toEqual(true);
   });
 
   it('should read epub zip from file path', () => {
-    const epubFilePath = './examples/Lorem ipsum.epub';
     const buffer = fs.readFileSync(epubFilePath);
     const book = new Book(buffer);
     expect(book.isZipFile).toEqual(true);
