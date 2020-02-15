68598c2508da125dc36f18d5e31fe1990c4d6d22
Richard <richard@stromer.org>
2020-02-15 15:16:01 +0100

    add two more tests for book creation based on epub-zip and buffer

--- src/book.spec.ts
+++ src/book.spec.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import Book from './book';
 
 describe('epub30-test-0100 - Tests for Content Documents in a reflowable context', () => {
@@ -5,5 +7,20 @@ describe('epub30-test-0100 - Tests for Content Documents in a reflowable context
     const epubFolderPath = './testsuite/content/30/epub30-test-0100';
     const book = new Book(epubFolderPath);
     expect(book.isZipFile).toEqual(false);
+  });
+});
+
+describe('Lorem ipsum - Epub zip-file created with Apple Pages', () => {
+  it('should read epub zip from file path', () => {
+    const epubFilePath = './examples/Lorem ipsum.epub';
+    const book = new Book(epubFilePath);
+    expect(book.isZipFile).toEqual(true);
+  });
+
+  it('should read epub zip from file path', () => {
+    const epubFilePath = './examples/Lorem ipsum.epub';
+    const buffer = fs.readFileSync(epubFilePath);
+    const book = new Book(buffer);
+    expect(book.isZipFile).toEqual(true);
   });
 });
