9f794312cc257de867134e8c56da017b42f66d0e
Richard <richard@stromer.org>
2020-02-15 15:55:13 +0100

    add test cases for title

--- src/book.spec.ts
+++ src/book.spec.ts
@@ -9,6 +9,11 @@ describe('epub30-test-0100 - Tests for Content Documents in a reflowable context
     const book = new Book(epubFolderPath);
     expect(book.isZipFile).toEqual(false);
   });
+
+  it('should have the right title', () => {
+    const book = new Book(epubFolderPath);
+    expect(book.title).toEqual('EPUBTEST 0100 - Reflowable Content Tests');
+  });
 });
 
 describe('Lorem ipsum - Epub zip-file created with Apple Pages', () => {
@@ -23,5 +28,10 @@ describe('Lorem ipsum - Epub zip-file created with Apple Pages', () => {
     const buffer = fs.readFileSync(epubFilePath);
     const book = new Book(buffer);
     expect(book.isZipFile).toEqual(true);
+  });
+
+  it('should have the right title', () => {
+    const book = new Book(epubFilePath);
+    expect(book.title).toEqual('Lorem ipsum');
   });
 });
