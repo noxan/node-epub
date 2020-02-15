525f29a563e9b0c2a9ff1d8ab231d711a3d984f9
Richard <richard@stromer.org>
2020-02-15 15:41:44 +0100

    add readAsText function to file storage

--- src/file-storage.ts
+++ src/file-storage.ts
@@ -1,12 +1,20 @@
+import path from 'path';
+import fs from 'fs';
 import Zip from 'adm-zip';
 
-export default interface FileStorage {}
+export default interface FileStorage {
+  readAsText(fileName: string, encoding?: string): string;
+}
 
 export class ZipFileStorage implements FileStorage {
   private zip: Zip;
 
   constructor(fileNameOrRawData: string | Buffer) {
     this.zip = new Zip(fileNameOrRawData);
+  }
+
+  readAsText(fileName: string, encoding = 'utf-8'): string {
+    return this.zip.readAsText(fileName, encoding);
   }
 }
 
@@ -15,5 +23,10 @@ export class DirectoryFileStorage implements FileStorage {
 
   constructor(directoryPath: string) {
     this.rootDirectory = directoryPath;
+  }
+
+  readAsText(fileName: string, encoding = 'utf-8') {
+    const filePath = path.join(this.rootDirectory, fileName);
+    return fs.readFileSync(filePath, { encoding });
   }
 }
