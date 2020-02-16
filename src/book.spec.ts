20a73fa6c73e2ebb7890635e24474cf18298e881
Richard <richard@stromer.org>
2020-02-16 13:42:39 +0100

    add test suite for Die Verwandlung - Franz Kafka

--- src/book.spec.ts
+++ src/book.spec.ts
@@ -78,6 +78,56 @@ describe('epub30-test-0130 - Tests for right-to-left page progression and vertic
   });
 });
 
+describe('Die Verwandlung - Franz Kafka - Epub zip-file', () => {
+  const epubFilePath = './examples/Die Verwandlung - Franz Kafka.epub';
+
+  it('should read epub zip from file path', () => {
+    const book = new Book(epubFilePath);
+    expect(book.isZipFile).toEqual(true);
+  });
+
+  it('should read epub zip from file path', () => {
+    const buffer = fs.readFileSync(epubFilePath);
+    const book = new Book(buffer);
+    expect(book.isZipFile).toEqual(true);
+  });
+
+  it('should have the right title', () => {
+    const book = new Book(epubFilePath);
+    expect(book.title).toEqual('Die Verwandlung');
+  });
+
+  it('should have one or multiple creators', () => {
+    const book = new Book(epubFilePath);
+    expect(book.creator).toEqual([
+      {
+        meta: {},
+        role: 'aut',
+        text: 'Franz Kafka',
+      },
+    ]);
+  });
+
+  it('should have one or multiple languages', () => {
+    const book = new Book(epubFilePath);
+    expect(book.language).toEqual(['nl']);
+  });
+
+  it('should have one or multiple identifier', () => {
+    const book = new Book(epubFilePath);
+    expect(book.identifier).toEqual({
+      id: 'BookId',
+      scheme: 'UUID',
+      text: 'urn:uuid:83b5dbdc-fc4b-4efe-9efe-ca949c7f0d54',
+    });
+  });
+
+  it('should have one or multiple coverimage', () => {
+    const book = new Book(epubFilePath);
+    expect(book.coverimage).toEqual({});
+  });
+});
+
 describe('Lorem ipsum - Epub zip-file created with Apple Pages', () => {
   const epubFilePath = './examples/Lorem ipsum.epub';
 
